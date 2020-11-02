5039ccf32b827333e8286a46021a31e74d0dcf6c
javgato <javier.gaton@alumnos.uva.es>
2020-11-02 19:48:06 +0100

    Capas de CompUsuario

---
+++ CompUsuario/CompUsuario-ejb/src/java/es/uva/dbcs/practica1/despliegue/CompUsuarioControlador.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uva.dbcs.practica1.despliegue;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Javier
+ */
+@Stateless
+public class CompUsuarioControlador implements CompUsuarioControladorLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
---
+++ CompUsuario/CompUsuario-ejb/src/java/es/uva/dbcs/practica1/despliegue/CompUsuarioControladorLocal.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uva.dbcs.practica1.despliegue;
+
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Javier
+ */
+@Local
+public interface CompUsuarioControladorLocal {
+    
+}
