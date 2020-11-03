6342a0a7f3ce3b6438b0bf13899536199ad6b5a6
javgato <javier.gaton@alumnos.uva.es>
2020-11-03 10:31:02 +0100

    WEB-INF anadida y modificacion CompUsuarioControlador

--- CompUsuario/CompUsuario-ejb/src/java/es/uva/dbcs/practica1/despliegue/CompUsuarioControlador.java
+++ CompUsuario/CompUsuario-ejb/src/java/es/uva/dbcs/practica1/despliegue/CompUsuarioControlador.java
@@ -8,6 +8,7 @@ package es.uva.dbcs.practica1.despliegue;
 import es.uva.dbcs.practica1.dominio.Empleado;
 import es.uva.dbcs.practica1.dominio.Empresa;
 import es.uva.dbcs.practica1.dominio.Usuario;
+import es.uva.dbcs.practica1.persistencia.EmpresaFacadeLocal;
 import es.uva.dbcs.practica1.persistencia.UsuarioFacadeLocal;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
@@ -18,6 +19,8 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class CompUsuarioControlador implements CompUsuarioControladorLocal {
+    @EJB
+    private EmpresaFacadeLocal eF;
     @EJB
     private UsuarioFacadeLocal uF;
 
@@ -43,11 +46,12 @@ public class CompUsuarioControlador implements CompUsuarioControladorLocal {
 
     @Override
     public Empresa getEmpresa(String nifcif) {
-        Empresa emp = null;
+        /*Empresa emp = null;
         Usuario u = uF.find(nifcif);
         if(u!=null)
             emp = u.getEmpresa();
-        return emp;
+        return emp;*/
+        return eF.find(nifcif);
     }
 
     @Override
